9bdcec34d463417a981786950196343c99a35197
bolin <113562503+bolin-j@users.noreply.github.com>
2022-09-15 10:01:08 +1000

    add a fruit pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # data handling
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')#pull the data into a dataframe
+
+# a pick list for people to pick fruit
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)#display the df on the front page
 
