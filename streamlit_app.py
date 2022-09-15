167194a1b398b7ed4273535f505032938067815f
bolin <113562503+bolin-j@users.noreply.github.com>
2022-09-15 10:02:52 +1000

    make the picker value more readerable

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # data handling
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')#pull the data into a dataframe
 
+#set the df index more readable
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # a pick list for people to pick fruit
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
